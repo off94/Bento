189a9bbc8e31d8e46d52df2fe397d68d9787b650
Óscar Fraca <oscar.fraca@kampal.com>
2026-03-04 01:11:28 +0100

    bypass own auto theme changer when theme is set to auto (externally managed)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -89,36 +89,38 @@ function inInterval(value, start, end) {
 	return valueMins >= startMins || valueMins <= endMins;
 }
 
-switch (CONFIG.autoChangeTheme) {
-	case 'os':
-		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			enableDark();
-		} else {
-			enableLight();
-		}
-		break;
-	case 'hour':
-		const date = new Date();
-		const month = date.getMonth();
-		const isWinter = month === 0 || month === 1 || month === 2;
-		const isSpring = month === 3 || month === 4 || month === 5;
-		const isSummer = month === 6 || month === 7 || month === 8;
-		const isFall = month === 9 || month === 10 || month === 11;
-		const hours = date.getHours() < 10 ? '0' + date.getHours().toString() : date.getHours().toString();
-		const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes().toString() : date.getMinutes().toString();
-		const currentTime = hours + ':' + minutes;
+if (theme !== 'auto') {
+	switch (CONFIG.autoChangeTheme) {
+		case 'os':
+			if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				enableDark();
+			} else {
+				enableLight();
+			}
+			break;
+		case 'hour':
+			const date = new Date();
+			const month = date.getMonth();
+			const isWinter = month === 0 || month === 1 || month === 2;
+			const isSpring = month === 3 || month === 4 || month === 5;
+			const isSummer = month === 6 || month === 7 || month === 8;
+			const isFall = month === 9 || month === 10 || month === 11;
+			const hours = date.getHours() < 10 ? '0' + date.getHours().toString() : date.getHours().toString();
+			const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes().toString() : date.getMinutes().toString();
+			const currentTime = hours + ':' + minutes;
 
-		if (isWinter && inInterval(currentTime, CONFIG.winterHourDarkThemeActive, CONFIG.winterHourDarkThemeInactive) ||
-			isSpring && inInterval(currentTime, CONFIG.springHourDarkThemeActive, CONFIG.springHourDarkThemeInactive) ||
-			isSummer && inInterval(currentTime, CONFIG.summerHourDarkThemeActive, CONFIG.summerHourDarkThemeInactive) ||
-			isFall && inInterval(currentTime, CONFIG.fallHourDarkThemeActive, CONFIG.fallHourDarkThemeInactive)
-		) {
-			enableDark();
-		} else {
-			enableLight();
-		}
-		break;
-	case 'no':
-	default:
-		break;
+			if (isWinter && inInterval(currentTime, CONFIG.winterHourDarkThemeActive, CONFIG.winterHourDarkThemeInactive) ||
+				isSpring && inInterval(currentTime, CONFIG.springHourDarkThemeActive, CONFIG.springHourDarkThemeInactive) ||
+				isSummer && inInterval(currentTime, CONFIG.summerHourDarkThemeActive, CONFIG.summerHourDarkThemeInactive) ||
+				isFall && inInterval(currentTime, CONFIG.fallHourDarkThemeActive, CONFIG.fallHourDarkThemeInactive)
+			) {
+				enableDark();
+			} else {
+				enableLight();
+			}
+			break;
+		case 'no':
+		default:
+			break;
+	}
 }
